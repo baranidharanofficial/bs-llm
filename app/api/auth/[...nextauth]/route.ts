a5a42ec134da3d32877f3380d5a4d2ebdf1eae41
Baranidharan <baranidharanofficial@gmail.com>
2023-10-24 11:59:39 +0530

    Fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,11 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-const clientId = "238573122369-c114efqurbsu2v91l31elr8tq0jhj4u1.apps.googleusercontent.com";
-const clientSecret = "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";
-
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: clientId ?? "",
-            clientSecret: clientSecret ?? ""
+            clientId: process.env.CLIENT_ID ?? "",
+            clientSecret: process.env.CLIENT_SECRET ?? ""
         })
     ],
     secret: process.env.NEXTAUTH_SECRET
